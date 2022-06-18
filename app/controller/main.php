37bd5833f867725268eddbb7325b508fd326a686
Alex180220 <59312394+Alex180220@users.noreply.github.com>
2022-06-18 16:06:54 +0300

    redo controller

--- app/controller/main.php
+++ app/controller/main.php
@@ -1,16 +1,16 @@
 <?php
 
-/*$uri = ltrim($_SERVER['REDIRECT_URL'], '/');
-if(isset($_REQUEST))
-{
-    $uri = $_REQUEST['uri'];
-}
-else $uri = ''; */
-if(filter_has_var(INPUT_GET, 'uri'))
-{
-    $uri = $_GET['uri'];
-}
-else $uri ='';
+$uri = ltrim($_SERVER['REDIRECT_URL'], '/');
+// if(isset($_REQUEST))
+// {
+//     $uri = $_REQUEST['uri'];
+// }
+// else $uri = ''; */
+// if(filter_has_var(INPUT_GET, 'uri'))
+// {
+//     $uri = $_GET['uri'];
+// }
+// else $uri ='';
 
 
 $servername = "localhost";
